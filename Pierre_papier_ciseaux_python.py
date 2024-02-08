f029b4240dec0fd888c7a5e39bf6740a9e5e8fc2
Thomas BOISAUBERT <thomascoca2002@gmail.com>
2024-02-08 20:11:52 +0100

    Refactor game logic and improve code formatting

--- Pierre_papier_ciseaux_python.py
+++ Pierre_papier_ciseaux_python.py
@@ -2,30 +2,29 @@ import random
 
 liste_choix = ["pierre", "papier", "ciseaux"]
 
-while True :
-
-    joueur = (input("Choisis entre pierre, papier, ciseaux : "))
+while True:
+    joueur = input("Choisis entre pierre, papier, ciseaux : ")
     ordinateur = random.choice(liste_choix)
 
-    if joueur == "pierre" and ordinateur == "pierre" :
-        print ("égalité")
-    elif joueur == "pierre" and ordinateur == "papier" :
-        print("perdu")
-    elif joueur == "pierre" and ordinateur == "ciseaux" :
-        print("gagné")
-    elif joueur == "ciseaux" and ordinateur == "ciseaux" :
-        print("égalité")
-    elif joueur == "ciseaux" and ordinateur == "papier" :
-        print("gagné")
-    elif joueur == "ciseaux" and ordinateur == "pierre" :
-        print("perdu")
-    elif joueur == "papier" and ordinateur == "papier" :
-        print("égalité")
-    elif joueur == "papier" and ordinateur == "ciseaux" :
-        print("perdu")
-    elif joueur == "papier" and ordinateur == "pierre" :
-        print("gagné")
+    if joueur == "pierre" and ordinateur == "pierre":
+        print("Égalité")
+    elif joueur == "pierre" and ordinateur == "papier":
+        print("Perdu")
+    elif joueur == "pierre" and ordinateur == "ciseaux":
+        print("Gagné")
+    elif joueur == "ciseaux" and ordinateur == "ciseaux":
+        print("Égalité")
+    elif joueur == "ciseaux" and ordinateur == "papier":
+        print("Gagné")
+    elif joueur == "ciseaux" and ordinateur == "pierre":
+        print("Perdu")
+    elif joueur == "papier" and ordinateur == "papier":
+        print("Égalité")
+    elif joueur == "papier" and ordinateur == "ciseaux":
+        print("Perdu")
+    elif joueur == "papier" and ordinateur == "pierre":
+        print("Gagné")
 
-    reponse = input("Voulez-vous jouer à nouveau ? (Oui/Non) : " )
-    if reponse.lower() != "oui" :
+    reponse = input("Voulez-vous jouer à nouveau ? (Oui/Non) : ")
+    if reponse.lower() != "oui":
         break
